aa2c4ed570d3d8227aacc17b819c1da8edd66e23
Ahmad Ridwan Fauzi <uzzysteam@gmail.com>
2019-09-06 14:20:19 +0900

    Added Seamless Chat

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,10 @@
 class Room
   include Mongoid::Document
+  include SimpleEnum::Mongoid
+
   field :name, type: String
+
+  as_enum :room_type, :private => 0, :group => 1, :public => 2
 
   has_many :room_messages, dependent: :destroy,
                          inverse_of: :room
