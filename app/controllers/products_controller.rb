1ece229592a8ac4b0f689421acbf1ed49993f098
KitaLabs, Co. Ltd <admin@kitalabs.com>
2019-09-07 14:20:00 +0900

    Added Working Price Filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :redirect_if_no_session, only: [:edit, :update, :destroy]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show]
 
   # GET /products
   # GET /products.json
@@ -16,7 +17,6 @@ class ProductsController < ApplicationController
   # GET /products/new
   def new
     @product = Product.new
-    Product.reindex
   end
 
   # GET /products/1/edit
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,16 @@
 class SearchController < ApplicationController
   def index
   	Product.reindex
-  	@products = Product.search(params[:q])
+
+
+  	if params.has_key?(:minprice) && (!params.has_key?(:maxprice) || params[:maxprice].empty?) 
+  		@products = Product.search params[:q], where: { price: { gt: params[:minprice] } }
+  	elsif (!params.has_key?(:minprice) || params[:minprice].empty?) && params.has_key?(:maxprice)
+  		@products = Product.search params[:q], where: { price: { lt: params[:maxprice] } }
+  	elsif params.has_key?(:minprice) && params.has_key?(:maxprice)
+  		@products = Product.search params[:q], where: { price: { gt: params[:minprice], lt: params[:maxprice] } }
+  	else
+  		@products = Product.search params[:q]
+  	end
   end
 end
